71c8166d31efd907a43058f4d2a4bcd10aba3b45
520MianXiangDuiXiang520 <15364968962@163.com>
2019-12-22 16:03:49 +0800

    告一段落

--- viewModel/money.js
+++ viewModel/money.js
@@ -14,7 +14,13 @@ let vm = new Vue({
     data: {
         who: 0,
         money: 0,
-        pay_back_date: "0000-00-00"
+        pay_back_date: "0000-00-00",
+        borrow_bills: [],
+        borrow_money : 0,
+        bank_money: 0,
+        bank_data: "",
+        bank_name: "",
+        is_add_to_family: false,
     },
     methods: {
         borrow() {
@@ -45,6 +51,94 @@ let vm = new Vue({
                     }
                 });
             }
+        },
+        get_borrow_bill() {
+            let self = this;
+            let token = getQueryVariable('token');
+            this.userToken = token;
+            if (token) {
+                reqwest({
+                    url: 'http://127.0.0.1:8000/api/v1/borrow_money/payBack/?token=' + token
+                    , method: 'get'
+                    , error(err) {
+                        alert("请求失败！")
+                    }
+                    , success(resp) {
+                        console.log("请求成功！");
+                        if (resp['code'] === 200) {
+                            self.borrow_bills = resp['data'];
+                            self.borrow_money = resp['data']['money']
+                        }else if(resp['code'] === 201) {
+                            self.borrow_bills = []
+                        } else {
+                            alert(resp['msg'])
+                        }
+
+                    }
+                });
+            }
+        },
+        payback(who, bill_id) {
+            let self = this;
+            let token = getQueryVariable('token');
+            this.userToken = token;
+            if (token) {
+                reqwest({
+                    url: 'http://127.0.0.1:8000/api/v1/borrow_money/payBack/?token=' + token
+                    , method: 'post'
+                    , type: 'json'
+                    , data: {
+                        who: who,
+                        money: self.borrow_money,
+                        bill_id: bill_id
+                    }
+                    , error(err) {
+                        alert("请求失败！")
+                    }
+                    , success(resp) {
+                        console.log("请求成功！");
+                        if (resp['code'] === 200) {
+                            alert("还款成功")
+                        } else {
+                            alert(resp['msg'])
+                        }
+
+                    }
+                });
+            }
+        },
+
+        bank() {
+            let self = this;
+            let token = getQueryVariable('token');
+            this.userToken = token;
+            let addFamily = 0;
+            if(self.is_add_to_family)
+                addFamily = 1;
+            if (token) {
+                reqwest({
+                    url: 'http://127.0.0.1:8000/api/v1/bill/Income/?token=' + token
+                    , method: "POST"
+                    , type: 'json'
+                    , data: {bill_type: 0,
+                        money: self.bank_money,
+                        is_add_to_family: addFamily,
+                        remarks: "存款到" + self.bank_name,
+                        time: self.bank_data + " 12:00:00",
+                        concrete_type: self.bank_name
+                    }
+                    , success: function (resp) {
+                        if(resp['code']===200) {
+                            alert("存款成功！");
+                        } else {
+                            alert(resp['msg'])
+                        }
+                    }
+                })
+            }
         }
+    },
+    mounted(){
+        this.get_borrow_bill();
     }
 });
